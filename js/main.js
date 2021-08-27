47f9815215431f034b340882519234ee6ad569a2
Simon Lindstedt <simon.lindstedt@gmail.com>
2021-08-27 13:26:42 +0200

    lots of fixes

--- js/main.js
+++ js/main.js
@@ -1,15 +1,9 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas#unity-canvas");
 const fullScreenButton = document.querySelector("button.full-screen");
 const preloader = document.querySelector(".preloader");
 const closeFullScreenButton = document.querySelector(
   ".full-screen-wrapper button"
 );
-
-function onStartup() {
-  setTimeout(() => {
-    preloader.remove();
-  }, 1100);
-}
 
 window.addEventListener("load", () => {
   ratioResize(16 / 9, canvas);
@@ -24,55 +18,30 @@ window.addEventListener("resize", () => {
   }
 });
 
-// About button
-const aboutButton = document.querySelector(".about-button");
-
-if (aboutButton) {
-  aboutButton.addEventListener("click", () => {
-    if (document.querySelector("section#about") === null) {
-      document.body.style.overflowY = "scroll";
-      renderAboutMenu();
-      document.querySelector("section#about").scrollIntoView({
-        behavior: "smooth",
-      });
-    } else {
-      document.querySelector("section#about").scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  });
+function onStartup() {
+  if (preloader) {
+    setTimeout(() => {
+      preloader.remove();
+    }, 1100);
+  }
 }
 
-// Fullscreen button listeners
+function onBackgroundChange() {
+  document.body.style.backgroundColor = "rgb(255,	209, 153)";
+  console.log("background change");
+}
 
-document.addEventListener("fullscreenchange", () => {
-  ratioResize(16 / 9, canvas);
-
-  if ((screen.width / screen.height).toFixed(4) === (16 / 9).toFixed(4)) {
-    canvas.style.width = `${window.innerWidth}px`;
-    canvas.style.height = `${window.innerHeight}px`;
-  }
-
-  if (document.fullscreenEnabled !== null) {
-    closeFullScreenButton.classList.toggle("active");
-  }
-});
-
-fullScreenButton.addEventListener("click", () => {
-  canvas.parentElement.requestFullscreen({ navigationUI: "show" });
-});
-
-closeFullScreenButton.addEventListener("click", () => {
-  document.exitFullscreen();
-});
+function onRestart() {
+  document.body.style.backgroundColor = "var(--bgColor)";
+}
 
 (async function () {
   const unityInstance = await createUnityInstance(
     canvas,
     {
-      dataUrl: "Build/Build.data",
-      frameworkUrl: "Build/Build.framework.js",
-      codeUrl: "Build/Build.wasm",
+      dataUrl: "Build/Build.data.unityweb",
+      frameworkUrl: "Build/Build.framework.js.unityweb",
+      codeUrl: "Build/Build.wasm.unityweb",
       streamingAssetsUrl: "StreamingAssets",
       companyName: "DefaultCompany",
       productName: "Sammanslaget",
@@ -89,5 +58,58 @@ closeFullScreenButton.addEventListener("click", () => {
       }
     }
   );
-  // unityInstance.SendMessage("JS", "SetText", "Hej hackathon");
+
+  // Fullscreen button listeners
+  document.addEventListener("fullscreenchange", () => {
+    ratioResize(16 / 9, canvas);
+
+    if ((screen.width / screen.height).toFixed(4) === (16 / 9).toFixed(4)) {
+      canvas.style.width = `${window.innerWidth}px`;
+      canvas.style.height = `${window.innerHeight}px`;
+    }
+
+    if (document.fullscreenEnabled !== null) {
+      closeFullScreenButton.classList.toggle("active");
+    }
+  });
+
+  fullScreenButton.addEventListener("click", () => {
+    canvas.parentElement.requestFullscreen({ navigationUI: "show" });
+  });
+
+  closeFullScreenButton.addEventListener("click", () => {
+    document.exitFullscreen();
+  });
+
+  // About button
+  const aboutButton = document.querySelector(".about-button");
+
+  if (aboutButton) {
+    aboutButton.addEventListener("click", () => {
+      unityInstance.SendMessage("JS", "SetMuteOverride", 1);
+      if (document.querySelector("section#about") === null) {
+        renderAboutMenu();
+
+        // Top button
+        const topButton = document.querySelector("button.return");
+        topButton.addEventListener("click", () => {
+          unityInstance.SendMessage("JS", "SetMuteOverride", 0);
+          document.body.style.overflowY = "hidden";
+          document.querySelector("main").style.display = "initial";
+          document.querySelector("html").style.scrollBehavior = "auto";
+          document.querySelector("section#about").scrollIntoView();
+          document.querySelector("html").style.scrollBehavior = "smooth";
+          document.querySelector("main").scrollIntoView({ behavior: "smooth" });
+        });
+      }
+      document.querySelector("section#about").scrollIntoView({
+        behavior: "smooth",
+      });
+      document.body.style.backgroundColor = "var(--bgColor)";
+      setTimeout(() => {
+        document.querySelector("main").style.display = "none";
+        document.body.style.overflowY = "scroll";
+      }, 1000);
+    });
+  }
 })();
